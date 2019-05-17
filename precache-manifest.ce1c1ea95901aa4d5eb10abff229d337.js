5961105c86bcb6bafc6237366cae70f9cce3ada4
Azenga Kevin <toomuch573@gmail.com>
2019-05-17 20:14:47 +0300

    Updates

---
+++ precache-manifest.ce1c1ea95901aa4d5eb10abff229d337.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f1afa979986a10149d437285abd0e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4053e440b44e4ec2acda",
+    "url": "/static/css/main.edebc3ef.chunk.css"
+  },
+  {
+    "revision": "c62efcaa75ed36a3c4f8",
+    "url": "/static/js/2.7531a37d.chunk.js"
+  },
+  {
+    "revision": "4053e440b44e4ec2acda",
+    "url": "/static/js/main.df2bf112.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
